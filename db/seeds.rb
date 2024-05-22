3f8d15012f471804a6a87c6b8063e29e90bd22ad
victor dupont <victordupont1407@gmail.com>
2024-05-22 17:01:41 +0200

    exo 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+
+# Générer 5 restaurants avec des données aléatoires
+10.times do
+  flat = Flat.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    price_per_night: Faker::Number.between(from: 50, to: 500),
+    number_of_guests: Faker::Number.between(from: 1, to: 10)
+  )
+  puts "Restaurant #{flat.name} created!"
+end
